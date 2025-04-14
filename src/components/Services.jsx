4f77a4100454f3d8ffc036b4da00f119709d022b
Ayo Bankole <bankoleayodele147@gmail.com>
2025-04-14 13:52:11 +0100

    full functional SEO responsive website

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -4,7 +4,7 @@ import { services1, services2 } from '../assets/assets';
 const Services = () => {
   return (
     <div className='bg-gray-950'>
-      <div className='md:mx-20 my-10 md:flex-row flex gap-12'>
+      <div className='md:mx-20 my-10 md:flex-row md:flex lg:flex gap-12'>
           <div className=' flex flex-col mt-20 gap-6'>
             <div className='text-left text-2xl pt-10'>
               <p className='text-white text-sm font-medium'>WHAT WE DO</p>
@@ -32,7 +32,7 @@ const Services = () => {
       </div>
       <div className='border border-b-gray-500 md:mx-20'></div>
 
-      <div className='md:mx-20 my-10 md:flex-row flex gap-12'>
+      <div className='md:mx-20 my-10 md:flex-row md:flex lg:flex gap-12'>
           
           <div className='flex flex-col items-center gap-4 my-6 md:mx-10'>     
             <div className='w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-4 pt-5 gap-y-6 px-3 sm:px-0'>
@@ -48,7 +48,6 @@ const Services = () => {
             </div>
           </div>
             
-              
             <div className='flex flex-col gap-6'>
               <p className='text-gray-500 mt-5'>Adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim.</p>
               <div className='text-left text-2xl pt-10'>
